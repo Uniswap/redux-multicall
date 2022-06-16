e01a5c66e2a6dca8a86ac6b5c754b267f65aeb94
Lint Action <lint-action@samuelmeuli.com>
2022-06-16 15:40:12 +0000

    Fix code style issues with Prettier

--- integration-tests/MultichainApp.tsx
+++ integration-tests/MultichainApp.tsx
@@ -14,7 +14,7 @@ export function MultichainApp() {
   const blocks = useMemo(() => [blockNumberMainnet, blockNumberRinkeby], [blockNumberMainnet, blockNumberRinkeby])
   return (
     <Provider store={store}>
-      <Updater chainId={ChainId.MAINNET} blockNumber={blockNumberMainnet}/>
+      <Updater chainId={ChainId.MAINNET} blockNumber={blockNumberMainnet} />
       <Updater chainId={ChainId.RINKEBY} blockNumber={blockNumberRinkeby} blocksPerFetch={5} />
       <Home chainIds={chains} blockNumbers={blocks} />
     </Provider>
