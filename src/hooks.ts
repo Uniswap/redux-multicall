23c79a2ba0a76d33fe264afa5f691e8646b22c83
Lynn Yu <lynn.yu@uniswap.org>
2022-06-29 13:53:29 -0400

    fix: fix hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -165,7 +165,7 @@ export function useSingleContractMultipleData(
   contract: Contract | null | undefined,
   methodName: string,
   callInputs: OptionalMethodInputs[],
-  options: Partial<ListenerOptionsWithGas> | undefined
+  options?: Partial<ListenerOptionsWithGas>
 ): CallState[] {
   const { gasRequired } = options ?? {}
 
