519335a56508e6c06f8d2ce42d73fb11a7818b6a
Zach Pomerantz <zzmp@uniswap.org>
2022-11-09 12:01:29 -0800

    build: clean up actions

--- integration-tests/App.test.tsx
+++ integration-tests/App.test.tsx
@@ -18,30 +18,32 @@ describe('Use multicall in test application', () => {
 
   it('Performs a single contract multicall query', async () => {
     render(<App />)
+
     // Check that block timestamp is correctly retrieved from block
-    const timestamp1 = await waitFor(() => screen.getByTestId('blockTimestamp'), { timeout: 20_000 /* 20 seconds */ })
+    const timestamp1 = await waitFor(() => screen.getByTestId('blockTimestamp'), { timeout: 20_000 })
     expect(timestamp1 && timestamp1?.textContent).toBeTruthy()
     const value1 = parseInt(timestamp1.textContent!, 10) * 1000
     const now1 = Date.now()
     expect(now1 - value1).toBeLessThan(MAX_BLOCK_AGE)
 
     // Wait for an updated block timestamp
-    await sleep(20_000 /* 20 seconds */)
+    await sleep(12_000)
 
     // Check that the block timestamp has updated correctly
-    const timestamp2 = await waitFor(() => screen.getByTestId('blockTimestamp'), { timeout: 1_000 /* 1 second */ })
+    const timestamp2 = await waitFor(() => screen.getByTestId('blockTimestamp'), { timeout: 1_000 })
     expect(timestamp2 && timestamp2.textContent).toBeTruthy()
     const value2 = parseInt(timestamp1.textContent!, 10) * 1000
     const now2 = Date.now()
     expect(now2 - value2).toBeLessThan(MAX_BLOCK_AGE)
-  }, 50_000 /* 50 seconds */)
+  }, 35_000)
 
   it('Performs a multi contract multicall query', async () => {
     render(<App />)
+
     // Check that max token balance is correctly retrieved
-    const balance = await waitFor(() => screen.getByTestId('maxTokenBalance'), { timeout: 20_000 /* 20 seconds */ })
+    const balance = await waitFor(() => screen.getByTestId('maxTokenBalance'), { timeout: 20_000 })
     expect(balance && balance?.textContent).toBeTruthy()
     const value1 = BigNumber.from(balance.textContent)
     expect(value1.gt(0)).toBeTruthy()
-  }, 25_000 /* 25 seconds */)
+  }, 25_000)
 })
--- integration-tests/MultichainApp.test.tsx
+++ integration-tests/MultichainApp.test.tsx
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom'
-import { act } from 'react-dom/test-utils'
 import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import { MultichainApp } from './MultichainApp'
@@ -7,11 +6,14 @@ import { sleep } from './utils'
 
 const MAX_BLOCK_AGE = 600_000 // 10 minutes
 
+function parseTimestamp(value: string | null) {
+  if (!value) throw new Error('No timestamp to parse')
+  return value.split(',').map((v) => parseInt(v, 10) * 1000)
+}
+
 describe('Use multicall in test multichain application', () => {
   it('Renders correctly initially', () => {
-    act(() => {
-      render(<MultichainApp />)
-    })
+    render(<MultichainApp />)
     const h1 = screen.getByText('Hello Multichain Multicall') // H1 in Home
     expect(h1).toBeTruthy()
     const missing = screen.queryByText('Does Not Exist')
@@ -19,11 +21,10 @@ describe('Use multicall in test multichain application', () => {
   })
 
   it('Performs a multichain single contract multicall query', async () => {
-    act(() => {
-      render(<MultichainApp />)
-    })
+    render(<MultichainApp />)
+
     // Check that block timestamp is correctly retrieved from block
-    const timestamps1 = await waitFor(() => screen.getByTestId('blockTimestamps'), { timeout: 20_000 /* 20 seconds */ })
+    const timestamps1 = await waitFor(() => screen.getByTestId('blockTimestamps'), { timeout: 20_000 })
     expect(timestamps1 && timestamps1?.textContent).toBeTruthy()
     const values1 = parseTimestamp(timestamps1.textContent)
     const now1 = Date.now()
@@ -32,20 +33,15 @@ describe('Use multicall in test multichain application', () => {
     expect(now1 - values1[1]).toBeLessThan(MAX_BLOCK_AGE)
 
     // Wait for an updated block timestamp
-    await sleep(20_000 /* 20 seconds */)
+    await sleep(12_000)
 
     // Check that the block timestamp has updated correctly
-    const timestamps2 = await waitFor(() => screen.getByTestId('blockTimestamps'), { timeout: 1_000 /* 1 second */ })
+    const timestamps2 = await waitFor(() => screen.getByTestId('blockTimestamps'), { timeout: 1_000 })
     expect(timestamps2 && timestamps2.textContent).toBeTruthy()
     const values2 = parseTimestamp(timestamps2.textContent)
     const now2 = Date.now()
     expect(values2.length).toEqual(2)
     expect(now2 - values2[0]).toBeLessThan(MAX_BLOCK_AGE)
     expect(now2 - values2[1]).toBeLessThan(MAX_BLOCK_AGE)
-  }, 50_000 /* 50 seconds */)
+  }, 35_000)
 })
-
-function parseTimestamp(value: string | null) {
-  if (!value) throw new Error('No timestamp to parse')
-  return value.split(',').map((v) => parseInt(v, 10) * 1000)
-}
